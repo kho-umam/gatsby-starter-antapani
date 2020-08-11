25960d031fd33b83ba9b021567a6d0779c225953
Khoirul Umam <flowchart.inc@gmail.com>
2020-08-11 09:13:40 +0700

    add /blog/ path to posts

--- gatsby-node.js
+++ gatsby-node.js
@@ -38,16 +38,16 @@ exports.createPages = async ({ graphql, actions }) => {
     const previous = index === posts.length - 1 ? null : posts[index + 1].node
     const next = index === 0 ? null : posts[index - 1].node
 
-    createPage({
-      path: post.node.fields.slug,
-      component: blogPost,
-      context: {
-        slug: post.node.fields.slug,
-        previous,
-        next,
-      },
+      createPage({
+        path: `blog${post.node.fields.slug}`,
+        component: blogPost,
+        context: {
+          slug: post.node.fields.slug,
+          previous,
+          next,
+        },
+      })
     })
-  })
 }
 
 exports.onCreateNode = ({ node, actions, getNode }) => {
