c4fe19f511a9a225e7c3ecd63a15db9fcfb344b8
Khoirul Umam <flowchart.inc@gmail.com>
2020-08-29 11:02:03 +0700

    add styled components

--- gatsby-config.js
+++ gatsby-config.js
@@ -32,6 +32,7 @@ module.exports = {
     `gatsby-plugin-root-import`,
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
+    `gatsby-plugin-styled-components`,
     {
       resolve: `gatsby-source-filesystem`,
       options: {
